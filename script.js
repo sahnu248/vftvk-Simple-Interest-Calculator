c1d3d4278500ccd341b4271535c65f82080b3fc0
sahnu248 <101447051+sahnu248@users.noreply.github.com>
2022-03-15 12:21:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,15 @@ function compute()
     function validatePrincipal()
 {
     var prval = document.getElementById("principal").value;
-    if ( prval === 0 || prval < 0 )
+    if ( prval < 0 )
     {
         alert("Enter a positive number"); 
+        return;
+    }
+    else if ( prval == 0 )
+    {
+        alert("Principal cannot be zero. Enter a positive number")
+        return;
     }
 }
     
